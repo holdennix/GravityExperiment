581c425067f90285dac547b46959cc11f182ece8
Holden Nix <holden@holdennix.com>
2021-05-29 16:19:49 -0700

    Added main program

---
+++ main.py
@@ -0,0 +1,31 @@
+import serial
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+port = "COM3"
+ser = serial.Serial(port, 9600)
+
+times = []
+
+for x in range(7):
+    s = ser.readline().decode().rstrip()
+    times.append(float(s))
+    print(s)
+
+times.pop(0)
+
+times = [time - times[0] for time in times]
+y = [0, 0.1, 0.2, 0.3, 0.4, 0.5]
+
+plt.plot(times, y, "bo")
+plt.yticks(y)
+plt.xticks(np.arange(min(times), max(times), 10))
+plt.xlabel("Time, t (ms)")
+plt.ylabel("Height, y, (m)")
+plt.title("Height vs Time graph")
+plt.show()
+
+gravity = (2 * 0.5) / times[-1]**2
+
+print(times)
